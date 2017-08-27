83ea181a40ed7d8a5f9daedc2695ee7e9ad5a78f
FireSnakeGuy <brianmacdonald@live.com>
2017-08-27 16:11:18 -0400

    Add files via upload

---
+++ MacDonald Portfolio/SubDet.php
@@ -0,0 +1,13 @@
+
+
+
+<?php
+
+$first-name = $_REQUEST['first-name'];
+$last-name = $_REQUEST['last-name'];
+$email = $_REQUEST['email'];
+
+echo("First Name:  " . $first-name);
+echo("<br/>Last Name:  " . $last-name);
+echo("<br/>Email Address:  " . $email);
+?>
---
+++ MacDonald Portfolio/fonts/Readme.txt
@@ -0,0 +1,5 @@
+TrueTypeFont: Jayadhira LILA EE01 (Regular)
+Copyright 2011 Kirtikhacara Jayadhira. All rights reserved
+www.jayadhira.org
+
+Inspired by handwriting of Gerald Scarfe. You can use this font for personal use freely.
